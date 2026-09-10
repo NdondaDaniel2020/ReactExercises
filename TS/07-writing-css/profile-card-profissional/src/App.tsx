4db4487607dab9f7dd0098b4945344642829e242
NdondaDaniel2020 <ndondadaniel2020@gmail.com>
2026-09-11 00:02:26 +0100

    feat: initialize React and TypeScript project with Tailwind CSS profile card component

---
+++ TS/07-writing-css/profile-card-profissional/src/App.tsx
@@ -0,0 +1,9 @@
+import { ProfileCard } from './ProfileCard';
+
+export default function App(): React.JSX.Element {
+  return (
+    <main className="bg-[#F6F6F6] flex min-h-screen justify-center items-center">
+      <ProfileCard />
+    </main>
+  );
+}
